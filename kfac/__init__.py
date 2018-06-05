b400f8da6bf946e74dcab19c7c68be5955c9fbdf
tvikram <tvikram@google.com>
2018-06-05 06:46:15 -0700

    \nInternal refactor\n
    PiperOrigin-RevId: 199289046

--- kfac/__init__.py
+++ kfac/__init__.py
@@ -28,8 +28,10 @@ from kfac.python.ops import loss_functions
 from kfac.python.ops import op_queue
 from kfac.python.ops import optimizer
 from kfac.python.ops import utils
+from kfac.python.ops.kfac_utils import periodic_inv_cov_update_kfac_opt
 from kfac.python.ops.tensormatch import graph_matcher
 from kfac.python.ops.tensormatch import graph_search
+
 # pylint: enable=unused-import,line-too-long
 
 # pylint: disable=invalid-name
