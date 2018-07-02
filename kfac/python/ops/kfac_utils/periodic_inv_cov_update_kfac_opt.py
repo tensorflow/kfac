c51b46d7bd2b3b0a868d7aa065eb5d58c97bc882
jamesmartens <jamesmartens@google.com>
2018-07-02 08:52:21 -0700

    \nInternal refactor\n
    PiperOrigin-RevId: 202957664

--- kfac/python/ops/kfac_utils/periodic_inv_cov_update_kfac_opt.py
+++ kfac/python/ops/kfac_utils/periodic_inv_cov_update_kfac_opt.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+# Dependency imports
+
 import tensorflow as tf
 
 from kfac.python.ops import optimizer
