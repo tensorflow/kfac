4fb902879a9613bf55687f109540b4609e5af8fb
jamesmartens <jamesmartens@google.com>
2018-11-12 12:41:57 -0800

    \nInternal refactor\n
    PiperOrigin-RevId: 221139216

--- kfac/python/ops/linear_operator.py
+++ kfac/python/ops/linear_operator.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""SmartMatrices definitions."""
+"""Extra functionality we need for LinearOperators."""
 
 from __future__ import absolute_import
 from __future__ import division
